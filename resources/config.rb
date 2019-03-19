f57c91512e2c85a36258c40cd48ea89b47a7bdb2
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2019-03-19 16:24:06 +0000

    Set the correct cookbook in the template.

--- resources/config.rb
+++ resources/config.rb
@@ -36,6 +36,7 @@ action :create do
   end
 
   template ::File.join(node['consul_template']['config_dir'], new_resource.name) do
+    cookbook 'consul-template-cookbook'
     source 'config-template.json.erb'
     user node['consul_template']['service_user']
     group node['consul_template']['service_group']
