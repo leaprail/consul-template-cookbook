00e91cdbe0ec9752f400f6440b6576ae458696e8
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2019-03-19 18:08:05 +0000

    Fix integration test.

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -4,10 +4,6 @@ template '/tmp/test.config.ctmpl' do
   source 'test.config.ctmpl.erb'
 end
 
-execute 'add test key/value' do
-  command "curl -X PUT -d 'something' http://localhost:8500/v1/kv/test"
-end
-
 consul_template_config 'test' do
   templates [{
     source: '/tmp/test.config.ctmpl',
@@ -16,4 +12,8 @@ consul_template_config 'test' do
     perms: 777,
   }]
   notifies :restart, 'service[consul-template]'
+end
+
+execute 'add test key/value' do
+  command "curl -X PUT -d 'something' http://localhost:8500/v1/kv/test"
 end
