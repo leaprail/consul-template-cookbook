3ed14107b5bd956930d17ec692d70b0db8d4f0af
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2019-04-02 17:18:34 +0100

    Increase version to see if supermarket updates the View Source.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name             'consul-template-cookbook'
+issues_url       'https://github.com/ist-dsi/consul-template-cookbook/issues'
+source_url       'https://github.com/ist-dsi/consul-template-cookbook'
 maintainer       'Simão Martins'
 maintainer_email 'simao.martins@tecnico.ulisboa.pt'
 license          'Apache-2.0'
 description      'Installs/Configures consul-template. This is a fork of the consul-template cookbook which fixes some errors and removes support for Windows.'
 long_description 'Installs/Configures consul-template'
-version          '0.1.0'
+version          '0.1.1'
+chef_version     '>= 14.10'
 
 recipe 'consul-template-cookbook', 'Installs, configures, and starts the consul-template service.'
 
@@ -15,7 +18,3 @@ supports 'centos', '>= 7.6'
 %w(tar poise-service).each do |dep|
   depends dep
 end
-
-issues_url 'https://github.com/ist-dsi/consul-template-cookbook/issues'
-source_url 'https://github.com/ist-dsi/consul-template-cookbook'
-chef_version '>= 14.10'
